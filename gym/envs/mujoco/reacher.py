821be476d45a0beda12079678a699ca2dc7fe10b
Adarsh <adarsh23.sehgal@gmail.com>
2018-04-30 14:29:34 -0700

    Door opening working

--- gym/envs/mujoco/reacher.py
+++ gym/envs/mujoco/reacher.py
@@ -1,6 +1,7 @@
 import numpy as np
 from gym import utils
 from gym.envs.mujoco import mujoco_env
+import math
 
 class ReacherEnv(mujoco_env.MujocoEnv, utils.EzPickle):
     def __init__(self):
@@ -13,14 +14,25 @@ class ReacherEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         reward_dist = - np.linalg.norm(vec)
 
 	#Reward for the action
-        reward_ctrl = - np.square(a).sum()
+        #reward_ctrl = - np.square(a).sum()
+        reward_ctrl = - np.transpose(a) * a
+        reward_ctrl = - np.square(reward_ctrl).sum()
+        #print(reward_ctrl)
 
 	#Reward for door opening
-	reward_door = 0;
+        vec1 = self.get_body_com("right_wheel_Link") - self.get_body_com("target") 
+        reward_door = np.linalg.norm(vec1)
+        #reward_door=math.pow(reward_door,5)
+        #reward_door = reward_door * reward_door
+        #print(self.get_body_com("door_surface"))
+        #print(reward_door)
 
 	#Total Reward
-        reward = reward_dist + reward_ctrl
-
+        w1=0.2
+        w2=0.2
+        w3=0.6
+        reward = w1*reward_dist + w2*reward_ctrl + w3*reward_door
+        
         self.do_simulation(a, self.frame_skip)
         ob = self._get_obs()
 
@@ -38,10 +50,10 @@ class ReacherEnv(mujoco_env.MujocoEnv, utils.EzPickle):
 
     def reset_model(self):
         #qpos = self.np_random.uniform(low=-0.1, high=0.1, size=self.model.nq) + self.init_qpos
-	qpos = self.init_qpos;
+        qpos = self.init_qpos
         while True:
-            self.goal = self.np_random.uniform(low=-.2, high=.2, size=2)
-            if np.linalg.norm(self.goal) < 2:
+            self.goal = self.np_random.uniform(low=-.1, high=.1, size=2)
+            if np.linalg.norm(self.goal) < 0.001:
                 break
         qpos[-2:] = self.goal
         qvel = self.init_qvel + self.np_random.uniform(low=-.005, high=.005, size=self.model.nv)
--- main.py
+++ main.py
@@ -36,7 +36,7 @@ flags.DEFINE_float('tau', 0.001, 'tau of soft target update')
 flags.DEFINE_float('discount', 0.99, 'discount factor of Q-learning')
 flags.DEFINE_float('learning_rate', 1e-3, 'value of learning rate')
 flags.DEFINE_integer('batch_size', 100, 'The size of batch for minibatch training')
-flags.DEFINE_integer('max_steps', 200, 'maximum # of steps for each episode')
+flags.DEFINE_integer('max_steps', 400, 'maximum # of steps for each episode')
 flags.DEFINE_integer('update_repeat', 10, 'maximum # of q-learning updates for each step')
 flags.DEFINE_integer('max_episodes', 10000, 'maximum # of episodes to train')
 
